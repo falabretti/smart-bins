0824cf938edaab5ab51cb18a080c36297161c890
Lucas Falabretti <lucasfalabretti7@gmail.com>
2022-11-26 20:41:33 -0300

    Add a table to show sensor history

--- web/src/pages/SensorDetails.tsx
+++ web/src/pages/SensorDetails.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import { useLocation } from "react-router-dom";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography, useTheme } from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
+import { getSensorRecords, SensorRecord } from "../services/client";
+
+type LocationState = {
+  sensorId: string;
+}
 
 function SensorDetails(props: any) {
 
   const { state } = useLocation();
+  const { sensorId } = state as LocationState;
+  const [records, setRecords] = useState<SensorRecord[]>([]);
+  const theme = useTheme();
+  const navigate = useNavigate();
 
-  console.log(state);
+  function loadRecords() {
+    getSensorRecords(sensorId)
+      .then(res => setRecords(res.data))
+      .catch(error => console.log(error));
+  }
+
+  function returnHome() {
+    navigate("/");
+  }
+
+  useEffect(() => {
+    loadRecords();
+  }, []);
 
   return (
-    <div>SensorDetails</div>
+    <>
+      <Button variant="contained" sx={{ mb: 2 }} onClick={returnHome}>Back</Button>
+      <TableContainer component={Paper}>
+        <Toolbar sx={{ [theme.breakpoints.up('xs')]: { pl: 2 } }}>
+          <Typography variant="h5" sx={{ fontWeight: 700 }}>Sensor {sensorId} History</Typography>
+        </Toolbar>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 600 }}>Sensor Id</TableCell>
+              <TableCell sx={{ fontWeight: 600 }} align="center">Location</TableCell>
+              <TableCell sx={{ fontWeight: 600 }} align="center">Timestamp</TableCell>
+              <TableCell sx={{ fontWeight: 600 }} align="center">Volume</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {records.map((record, key) => (
+              <TableRow key={key}>
+                <TableCell>{record.sensor_id}</TableCell>
+                <TableCell align="center">{record.location}</TableCell>
+                <TableCell align="center">{new Date(record.timestamp).toLocaleString()}</TableCell>
+                <TableCell align="center">{Math.trunc(record.volume * 100)}%</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
 
--- web/src/services/client.ts
+++ web/src/services/client.ts
@@ -5,9 +5,14 @@ const HOST = 'http://localhost:5000'
 export type SensorRecord = {
     sensor_id: string,
     location: string,
-    volume: number
+    volume: number,
+    timestamp: string
 }
 
 export async function getLatestRecords(): Promise<AxiosResponse<SensorRecord[]>> {
     return await axios.get(HOST + '/record?last=true');
+}
+
+export async function getSensorRecords(sensorId: string): Promise<AxiosResponse<SensorRecord[]>> {
+    return await axios.get(HOST + '/record?id=' + sensorId);
 }
