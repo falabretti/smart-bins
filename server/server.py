0e216f23844c5da96d0d8a40a17dfcbf9a9a9d68
Lucas Falabretti <lucasfalabretti7@gmail.com>
2022-11-27 03:07:32 -0300

    Add websocket on server to send events of new records

--- server/server.py
+++ server/server.py
@@ -1,18 +1,21 @@
 import client
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 
 SENSOR_ID_PARAM = 'id'
 LAST_PARAM = 'last'
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app,  cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 
 @app.route('/record', methods=['POST'])
 def create_record():
     record = request.get_json()
     client.write_record(record)
+    socketio.send(record)
     return '', 201
 
 
@@ -23,4 +26,4 @@ def retrieve_records():
     return jsonify(client.query_records(sensor_id=sensor_id, last_only=last_only))
 
 
-app.run(host="0.0.0.0", debug=True)
+socketio.run(app, debug=True, host='0.0.0.0', allow_unsafe_werkzeug=True)
